90590f64455eb5c98ac816b177388759c304b215
Neelima32158 <2400032158@kluniversity.in>
2026-01-22 10:26:06 +0530

    22-01-2026

---
+++ Di-ECommereceApp-Autowired/src/main/java/com/klu/DiECommereceAppAutowiredApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiECommereceAppAutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiECommereceAppAutowiredApplication.class, args);
+	}
+
+}
---
+++ Di-ECommereceApp-Autowired/src/main/java/com/klu/model/Product.java
@@ -0,0 +1,35 @@
+package com.klu.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Product {
+	private int productId;
+	private String productName;
+	private  double price;
+	private String category;
+	
+	public Product() {
+		this.productId=101;
+		this.productName="laptop";
+		this.price=450000.00;
+		this.category="Electonics";
+	}
+	public int getProductId() {
+		return productId;
+		
+	}
+	public String getProductName() {
+		return productName;
+		
+	}
+	public double getPrice() {
+		return price;
+		
+	}
+	public String getCategory() {
+		return category;
+	}
+	
+
+}
