d6ce4d9f88ae41b7fe3f3d8e38c22ea373729e55
Neelima32158 <2400032158@kluniversity.in>
2026-02-11 14:52:02 +0530

    11-02-2026

---
+++ SpringMVCDemoApp/src/main/java/com/klu/Controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.klu.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.klu.Service.MessageService;
+import com.klu.model.Message;
+
+@Controller
+public class HomeController {
+@Autowired
+private MessageService MessageService;
+@GetMapping("/")
+public String home(Model model) {
+  Message msg = new Message(MessageService.getMessage());
+      model.addAttribute("message",msg);
+  return "home";
+}
+
+}
